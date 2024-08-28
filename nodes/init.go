4382f7a5b5843276cded45ea5e7e53c772656096
Eric Hackborn <hackborn@gmail.com>
2024-08-27 17:27:54 -0700

    wip: A proper testing framework for drivers, so we're not relying on just glancing at some result text.

--- nodes/init.go
+++ nodes/init.go
@@ -1,12 +1,23 @@
 package nodes
 
 import (
+	"embed"
+
 	"github.com/hackborn/onefunc/pipeline"
 )
 
 func init() {
+	// Register the test data
+	pipeline.RegisterFs("testnodedata", testNodeDataFs)
+
 	pipeline.RegisterNode("compare", func() pipeline.Node {
 		return &compareReportsNode{}
+	})
+	pipeline.RegisterNode("testgen", func() pipeline.Node {
+		return newTestDocDriverNode("gen")
+	})
+	pipeline.RegisterNode("testref", func() pipeline.Node {
+		return newTestDocDriverNode("ref")
 	})
 	pipeline.RegisterNode("rungen", func() pipeline.Node {
 		return newRunDocDriverNode("gen")
@@ -15,3 +26,6 @@ func init() {
 		return newRunDocDriverNode("ref")
 	})
 }
+
+//go:embed testnodedata/*
+var testNodeDataFs embed.FS
---
+++ nodes/test_doc_driver_node.go
@@ -0,0 +1,151 @@
+package nodes
+
+import (
+	"cmp"
+	"encoding/json"
+	"fmt"
+
+	"github.com/hackborn/onefunc/jacl"
+	"github.com/hackborn/onefunc/pipeline"
+	"github.com/hackborn/onefunc/reflect"
+
+	"github.com/hackborn/doc"
+	"github.com/hackborn/doc_drivers/domain"
+	"github.com/hackborn/doc_drivers/registry"
+)
+
+func newTestDocDriverNode(docDriverPrefix string) pipeline.Node {
+	n := &testDocDriverNode{}
+	n.docDriverPrefix = docDriverPrefix
+	return n
+}
+
+type testDocDriverNode struct {
+	testDocDriverData
+}
+
+type testDocDriverData struct {
+	Verbose bool
+	Backend string
+
+	docDriverPrefix string
+}
+
+func (d *testDocDriverData) docDriverName() string {
+	return d.docDriverPrefix + "/" + d.Backend
+}
+
+func (n *testDocDriverNode) Start(input pipeline.StartInput) error {
+	data := n.testDocDriverData
+	input.SetNodeData(&data)
+	return nil
+}
+
+func (n *testDocDriverNode) Run(state *pipeline.State, input pipeline.RunInput, output *pipeline.RunOutput) error {
+	data := state.NodeData.(*testDocDriverData)
+	var err error
+	for _, pin := range input.Pins {
+		switch t := pin.Payload.(type) {
+		case *pipeline.ContentData:
+			err = cmp.Or(err, n.runContent(data, t))
+		}
+	}
+	return err
+}
+
+func (n *testDocDriverNode) runContent(data *testDocDriverData, cd *pipeline.ContentData) error {
+	// Load tests
+	entries := []testEntry{}
+	err := json.Unmarshal([]byte(cd.Data), &entries)
+	if err != nil {
+		return err
+	}
+
+	// Open the database
+	f, ok := registry.Find(data.Backend)
+	if !ok {
+		return fmt.Errorf("No backend named \"%v\"", data.Backend)
+	}
+	db, err := doc.Open(data.docDriverName(), f.DbPath)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	// Run tests
+	for _, te := range entries {
+		err = cmp.Or(err, n.runTest(db, te))
+	}
+	if err != nil {
+		err = fmt.Errorf("%v %w", cd.Name, err)
+	}
+	return err
+}
+
+func (n *testDocDriverNode) runTest(db *doc.DB, te testEntry) error {
+	switch te.Command {
+	case "get":
+		return n.runGetTest(db, te)
+	case "set":
+		return n.runSetTest(db, te)
+	default:
+		return fmt.Errorf("Unhandled test command \"%v\"", te.Command)
+	}
+}
+
+func (n *testDocDriverNode) runGetTest(db *doc.DB, te testEntry) error {
+	req := doc.GetRequest{}
+	var err error
+	req.Condition, err = db.Expr(te.Expr, nil).Compile()
+	if err != nil {
+		return err
+	}
+
+	switch te.Type {
+	case "Filing":
+		resp, err := doc.Get[domain.Filing](db, req)
+		if err != nil {
+			return err
+		}
+		return jacl.Run(resp.Results, te.Response...)
+	default:
+		return fmt.Errorf("Unhandled type \"%v\"", te.Type)
+	}
+}
+
+func (n *testDocDriverNode) runSetTest(db *doc.DB, te testEntry) error {
+	switch te.Type {
+	case "Filing":
+		fitem, err := newTestItem[domain.Filing](te.Item)
+		if err != nil {
+			return err
+		}
+		req := doc.SetRequest[domain.Filing]{Item: fitem}
+		resp, err := doc.Set(db, req)
+		// The API is currently unclear on whether a return item
+		// is required, but I think all the drivers ignore it right
+		// now so we'll just assume it's optional.
+		if resp.Item == nil {
+			return nil
+		}
+		return jacl.Run(resp.Item, te.Response...)
+	default:
+		return fmt.Errorf("Unhandled type \"%v\"", te.Type)
+	}
+}
+
+func newTestItem[T any](item map[string]any) (T, error) {
+	var t T
+	req := reflect.SetRequestFrom(item)
+	req.Flags = reflect.Fuzzy
+	err := reflect.Set(req, &t)
+	return t, err
+}
+
+type testEntry struct {
+	Command  string         `json:"command"`
+	Type     string         `json:"type"`
+	Expr     string         `json:"expr"`
+	Item     map[string]any `json:"item"`
+	Response []string       `json:"response"`
+}
